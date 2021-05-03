d27e10548eb3798a850761fd60a55c9b4db39ddc
Kelis-github <82428272+Kelis-github@users.noreply.github.com>
2021-05-03 00:32:22 -0400

    Commit2

--- MeanProject/src/MeanPackage/MeanClass.java
+++ MeanProject/src/MeanPackage/MeanClass.java
@@ -4,7 +4,22 @@ public class MeanClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Tring to Push and Commit to GitHub");
+		int number1=25;
+		int number2=50;
+		int result=number1+number2;
+		System.out.println("The sum of number1 and number2="+result);
+		//computes the sample variance
+		int[] sampleValues= {10,20,50,30,70};
+		int sampleSize=sampleValues.length;
+		int n=sampleSize;
+		int sum=0,i;
+		 for(i = 0; i < n; i++) {
+	         sum+=sampleValues[i];
+		 }
+	     System.out.println("Sample size,n="+n);  
+	     System.out.println("Sum of Observation="+sum);  
+	     System.out.println("MEAN="+sum/n);  
+	     System.out.println("Tring to Push and Commit to GitHub");
 	}
 
 }
