90e7a3d2c1331a61b748754c98f1464e9cba9697
Kelis-github <82428272+Kelis-github@users.noreply.github.com>
2021-05-03 00:19:30 -0400

    Commit1

---
+++ MeanProject/src/MeanPackage/MeanClass.java
@@ -0,0 +1,10 @@
+package MeanPackage;
+
+public class MeanClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Tring to Push and Commit to GitHub");
+	}
+
+}
